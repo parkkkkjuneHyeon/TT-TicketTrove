60d127d6bc8f66fd60d7ac52fbf5d2b56d043a07
박준현 <gktkwk91@naver.com>
2024-07-19 17:36:15 +0900

    [프로젝트 세팅] 프로젝트 init

---
+++ src/main/java/com/trove/ticket_trove/TicketTroveApplication.java
@@ -0,0 +1,12 @@
+package com.trove.ticket_trove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketTroveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketTroveApplication.class, args);
+    }
+}
