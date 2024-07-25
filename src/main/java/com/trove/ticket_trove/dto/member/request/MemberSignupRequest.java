a7c1fe7854ea22b5d58402453e81c5003360928d
박준현 <gktkwk91@naver.com>
2024-07-25 14:29:51 +0900

    [DTO 수정] memberDTO validation 추가

--- src/main/java/com/trove/ticket_trove/dto/member/request/MemberSignupRequest.java
+++ src/main/java/com/trove/ticket_trove/dto/member/request/MemberSignupRequest.java
@@ -1,19 +1,18 @@
 package com.trove.ticket_trove.dto.member.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record MemberSignupRequest(
-    String name,
-    @Email
-    String email,
-    String password,
-    @Size(min = 1, max = 1)
-    String gender,
-    @Min(0)
-    @Max(150)
-    Integer age) {
+        @NotBlank
+        String name,
+        @Email
+        String email,
+        String password,
+        @Size(min = 1, max = 1)
+        String gender,
+        @Min(0)
+        @Max(150)
+        Integer age
+) {
 
 }
