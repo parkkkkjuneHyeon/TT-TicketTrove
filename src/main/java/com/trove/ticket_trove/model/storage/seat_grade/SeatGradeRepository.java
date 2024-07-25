ffef689368e33226c3ea988ee00b352ab16c8ad3
박준현 <gktkwk91@naver.com>
2024-07-25 14:18:52 +0900

    [엔티티 구현] SeatGradeEntity와 SeatGradeRepository 구현

---
+++ src/main/java/com/trove/ticket_trove/model/storage/seat_grade/SeatGradeRepository.java
@@ -0,0 +1,15 @@
+package com.trove.ticket_trove.model.storage.seat_grade;
+
+import com.trove.ticket_trove.model.entity.concert.ConcertEntity;
+import com.trove.ticket_trove.model.entity.seat_grade.SeatGradeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SeatGradeRepository extends JpaRepository<SeatGradeEntity, Long> {
+    Optional<SeatGradeEntity> findByConcertIdAndGrade(ConcertEntity concertEntity, String grade);
+
+    void deleteAllByConcertId(ConcertEntity concertEntity);
+}
