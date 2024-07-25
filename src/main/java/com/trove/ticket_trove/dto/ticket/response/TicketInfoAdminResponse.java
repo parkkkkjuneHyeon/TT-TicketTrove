cba13704ea02f4b3f5d66d4153dbbf305484775c
박준현 <gktkwk91@naver.com>
2024-07-25 14:33:06 +0900

    [DTO 추가] ticketDTO 추가

---
+++ src/main/java/com/trove/ticket_trove/dto/ticket/response/TicketInfoAdminResponse.java
@@ -0,0 +1,39 @@
+package com.trove.ticket_trove.dto.ticket.response;
+
+import com.trove.ticket_trove.model.entity.ticket.TicketEntity;
+
+import java.time.LocalDateTime;
+
+public record TicketInfoAdminResponse(
+        Long ticketId,
+        String name,
+        String email,
+        String concertName,
+        String performer,
+        String grade,
+        Integer seatNumber,
+        Integer price,
+        LocalDateTime showStart,
+        LocalDateTime showEnd,
+        LocalDateTime createdAt,
+        LocalDateTime deletedAt
+) {
+    public static TicketInfoAdminResponse from(
+            TicketEntity ticketEntity
+    ) {
+        var concertEntity = ticketEntity.getConcertId();
+        return new TicketInfoAdminResponse(
+                ticketEntity.getId(),
+                ticketEntity.getMemberEmail().getName(),
+                ticketEntity.getMemberEmail().getEmail(),
+                concertEntity.getConcertName(),
+                concertEntity.getPerformer(),
+                ticketEntity.getSeatGrade().getGrade(),
+                ticketEntity.getSeatNumber(),
+                ticketEntity.getSeatGrade().getPrice(),
+                concertEntity.getShowStart(),
+                concertEntity.getShowEnd(),
+                ticketEntity.getCreatedAt(),
+                ticketEntity.getDeletedAt());
+    }
+}
---
+++ src/main/java/com/trove/ticket_trove/dto/ticket/response/TicketInfoResponse.java
@@ -0,0 +1,31 @@
+package com.trove.ticket_trove.dto.ticket.response;
+
+import com.trove.ticket_trove.model.entity.ticket.TicketEntity;
+
+import java.time.LocalDateTime;
+
+public record TicketInfoResponse(
+        Long id,
+        String name,
+        String concertName,
+        String performer,
+        String grade,
+        Integer seatNumber,
+        LocalDateTime showStart,
+        LocalDateTime showEnd
+) {
+    public static TicketInfoResponse from(
+            TicketEntity ticketEntity
+    ) {
+        var concertEntity = ticketEntity.getConcertId();
+        return new TicketInfoResponse(
+                ticketEntity.getId(),
+                ticketEntity.getMemberEmail().getName(),
+                concertEntity.getConcertName(),
+                concertEntity.getPerformer(),
+                ticketEntity.getSeatGrade().getGrade(),
+                ticketEntity.getSeatNumber(),
+                concertEntity.getShowStart(),
+                concertEntity.getShowEnd());
+    }
+}
